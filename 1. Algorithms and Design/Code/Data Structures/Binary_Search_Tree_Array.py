c4263f8908cba543e06de4d06d718b5dc50ae717
Chen Xihao <chenxihao0627@gmail.com>
2018-08-17 13:09:52 +0800

    Updated Binary_Search_Tree_Array.py.

--- 1. Algorithms and Design/Code/Data Structures/Binary_Search_Tree_Array.py
+++ 1. Algorithms and Design/Code/Data Structures/Binary_Search_Tree_Array.py
@@ -64,25 +64,25 @@ class tree:
 
 			return ""
 
-		else:												# tree is not empty
-			curr_node = self.tree[curr]					# assign pointer
+		else:													# tree is not empty
+			curr_node = self.tree[curr]							# assign pointer
 
-			if value < curr_node.data:				# compare values
-				if curr_node.left != 0:				# curr.left is not empty
+			if value < curr_node.data:							# compare values
+				if curr_node.left != 0:							# curr.left is not empty
 					self.add_recursive(value, curr_node.left)	# moves left
 
-				else:										# curr.left is empty
-					curr_node.left = target			# insert
+				else:											# curr.left is empty
+					curr_node.left = target						# insert
 					self.tree[target].data = value
 					self.nextfree = self.tree[target].right
 					self.tree[target].right = 0
 
-			else:											# compare values
-				if curr_node.right != 0:				# curr.right is not empty
+			else:												# compare values
+				if curr_node.right != 0:						# curr.right is not empty
 					self.add_recursive(value, curr_node.right)	# moves right
 
-				else:										# curr.right is empty
-					curr_node.right = target			# insert
+				else:											# curr.right is empty
+					curr_node.right = target					# insert
 					self.tree[target].data = value
 					self.nextfree = self.tree[target].left
 					self.tree[target].right = 0
