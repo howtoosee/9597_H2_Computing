ff863502efe022911261ca2ca401c8bf3ec35a5a
Chen Xihao <chenxihao0627@gmail.com>
2018-09-09 23:42:55 +0800

    slight update

--- 1. Algorithms and Design/Code/Sieve of Eratosthenes - Prime Num.py
+++ 1. Algorithms and Design/Code/Sieve of Eratosthenes - Prime Num.py
@@ -4,15 +4,15 @@ from math import sqrt
 num = int(input("enter number: "))
 set = list(range(num+1))
 
-for i in range(2, num+1):
-        set[i] = True
+for i in range(2, num+1):						# assume all to be primes
+    set[i] = True
 
-for m in range(2, int(sqrt(num)+1)):
+for m in range(2, int(sqrt(num)+1)):			# filter out non-primes
         if set[m] == True:
-                for n in range(m*m, num+1, m):
-                        set[n] = False
+            for n in range(m*m, num+1, m):		# (m*m) to increase efficiency
+                set[n] = False
 
 print("Primes: ", end = "")
-        for i in range(2, num+1):
-                if set[i] == True:
-                        print(i, end = " ")
+    for i in range(2, num+1):
+        if set[i] == True:
+            print(i, end = " ")
