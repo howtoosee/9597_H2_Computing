d4e21e154b537fde58af82bec939a952d8184f9e
Chen Xihao <chenxihao0627@gmail.com>
2018-10-02 18:03:37 +0800

    increased efficiency

--- 1. Algorithms and Design/Code/Sorting/InsertionSort.py
+++ 1. Algorithms and Design/Code/Sorting/InsertionSort.py
@@ -1,24 +1,16 @@
-def swap(arr, i, j):
-	temp = arr[i]
-	arr[i] = arr[j]
-	arr[j] = temp
-
-	return arr
-
-
 def insertionsort(array):
 	for i in range(len(array)):
 		target = array[i]
 		j = i
 
 		while j > 0 and target < array[j-1]:
-			array = swap(array, j, j-1)
+			array[j] = array[j-1]
 			j -= 1
 
 		array[j] = target
 
 	return array
-	
+
 
 def main():
 	arr = [10, 98, 71, 13, 76, 34, 51, 1, 0, 69, 22, 90]
